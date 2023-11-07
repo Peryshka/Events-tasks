c27a9359bc44b4cd7f26a735d6b16d75687ab706
Perishte <javascript_9595@bk.ru>
2023-11-07 23:14:43 +0500

    Events-tasks 1 task

---
+++ main.js
@@ -0,0 +1,27 @@
+/*
+1) Дан массив из объектов с полями title и text. Заполните массив сами произвольными данными.
+ Ваша задача отрисовать данные на странице в соответствующих тегах (h2 - для title, p - для text)
+*/
+// const articles = [
+//   {title: 'WCNC', text: 'Israel plans to maintain security in Gaza indefinitely, Netanyahu says'},
+//   {title: 'ABC News', text: 'Brazilian police search Portugal\'s Consulate in Rio de Janeiro for a corruption investigation'},
+//   {title: 'BBC News', text: 'King Charles III delivers first King’s Speech in more than 70 years to UK parliament - BBC News'},
+//   {title: 'Hindustan Times', text: 'Iran\'s Raisi Wants PM Modi\'s Help To End Israel\'s War In Gaza | \'India Is Expected To Use...'},
+//   {title: 'Daily Beast', text: 'Ahed Tamimi, 22, was detained as Gaza health officials said over 10,000 have died in Israeli attacks.'},
+//   {title: 'Reuters', text: 'Turkish parliament dumps Coke, Nestle from menus over alleged Israel support'},
+//   {title: 'Politico', text: 'Poland’s Duda nominates Morawiecki in no-hope bid to form a new government'},
+//   {title: 'CNA', text: 'China and Australia agree to build trust, overcome differences'}
+// ];
+//
+// articles.forEach((item,idx, array) => {
+//   let title = document.createElement('h2');
+//   let text = document.createElement('p');
+//     title.textContent = item.title;
+//     text.textContent = item.text;
+//     document.body.append(title);
+//     document.body.append(text);
+// });
+
+
+
+
